a4c57ea4d38dc5e67ebc30f0a71fdccec85444fe
ppotatoG <thgus2270@gmail.com>
2024-03-11 22:02:13 +0900

    Fix: ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,7 +52,9 @@ module.exports = {
       version: 'detect',
     },
     'import/resolver': {
-      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
   },
 };
