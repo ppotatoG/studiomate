207261bcf6c5d966ee3483ebafa33a26cc38f9b9
ppotatoG <thgus2270@gmail.com>
2024-03-18 15:48:43 +0900

    Refactor: usePokemonLoader

--- src/hooks/usePokemonLoader.ts
+++ src/hooks/usePokemonLoader.ts
@@ -1,74 +1,71 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { loadingState } from '../state/loadingState';
-import fetchPokemonData from '../utils/fetchPokemonData'; // fetchPokemonData 가져오기
+import fetchPokemonData from '../utils/fetchPokemonData';
 import formatPokemons from '../utils/formatPokemons';
 
 const usePokemonLoader = (searchTerm: string) => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [nextUrl, setNextUrl] = useState<string | null>(
+  const [nextUrl, setNextUrl] = useState<string>(
     'https://pokeapi.co/api/v2/pokemon?offset=0&limit=40'
   );
   const [isLoading, setIsLoading] = useRecoilState(loadingState);
 
-  const loadPokemons = async () => {
-    if (!nextUrl || isLoading) return;
-
+  const fetchPokemons = async (url: string) => {
     setIsLoading(true);
     try {
-      const response = await fetch(nextUrl);
-      const data = await response.json();
-      const formattedPokemons = formatPokemons(data.results);
-
-      setPokemons(prev => {
-        const newPokemons = formattedPokemons.filter(
-          newPokemon =>
-            !prev.some(prevPokemon => prevPokemon.id === newPokemon.id)
-        );
-        return [...prev, ...newPokemons];
-      });
-      setNextUrl(data.next);
+      const response = await fetch(url);
+      return await response.json();
     } catch (error) {
-      console.error('Error loading pokemons:', error);
+      console.error('Error fetching pokemons:', error);
+      return null;
     } finally {
       setIsLoading(false);
     }
   };
 
-  const searchPokemons = async () => {
-    if (searchTerm) {
-      setIsLoading(true);
-      const searchedPokemon = await fetchPokemonData(searchTerm);
-      if (searchedPokemon) {
-        setPokemons([searchedPokemon]);
-      } else {
-        setPokemons([]);
-      }
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
-    if (searchTerm) {
-      searchPokemons();
-    } else {
-      if (pokemons.length === 0 && nextUrl) {
-        loadPokemons();
+    const initLoad = async () => {
+      if (!searchTerm && pokemons.length === 0) {
+        const data = await fetchPokemons(nextUrl);
+        if (data) {
+          setPokemons(formatPokemons(data.results));
+          setNextUrl(data.next);
+        }
       }
-    }
+    };
+    initLoad();
   }, [searchTerm]);
 
   useEffect(() => {
-    const handleScroll = () => {
+    const searchPokemon = async () => {
+      if (searchTerm) {
+        const searchedPokemon = await fetchPokemonData(searchTerm);
+        if (searchedPokemon) {
+          setPokemons([searchedPokemon]);
+        } else {
+          setPokemons([]);
+        }
+      }
+    };
+    searchPokemon();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const handleScroll = async () => {
       if (
         window.innerHeight + document.documentElement.scrollTop !==
           document.documentElement.offsetHeight ||
-        isLoading
+        isLoading ||
+        searchTerm
       )
         return;
-      if (!searchTerm) {
-        loadPokemons();
+
+      const data = await fetchPokemons(nextUrl);
+      if (data) {
+        setPokemons(prev => [...prev, ...formatPokemons(data.results)]);
+        setNextUrl(data.next);
       }
     };
 
